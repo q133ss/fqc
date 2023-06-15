ecbb8e56970badf4ed87b970b03bfccc053ac2c8
Alexey <test@git.ru>
2023-06-15 19:10:08 +0300

    pages

--- wp-content/themes/fcq/pages/contact-page.php
+++ wp-content/themes/fcq/pages/contact-page.php
@@ -22,33 +22,7 @@ get_header();
 
             </div>
 
-            <div class="header__menu">
-
-              <div class="header__menu-item">
-
-                <a class="header__menu-link" href="/">Схема работы</a>
-
-              </div>
-
-              <div class="header__menu-item">
-
-                <a class="header__menu-link" href="/">Прайс</a>
-
-              </div>
-
-              <div class="header__menu-item">
-
-                <a class="header__menu-link" href="/">Доставка из китая</a>
-
-              </div>
-
-              <div class="header__menu-item">
-
-                <a class="header__menu-link" href="/">О нас</a>
-
-              </div>
-
-            </div>
+            <?php get_template_part('parts/menu'); ?>
 
           </div>
 
