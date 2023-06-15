f907199e6a46111dbf9d84695d69ac087cbada77
Alexey <test@git.ru>
2023-06-15 18:50:14 +0300

    contact page

---
+++ wp-content/themes/fcq/pages/contact-page.php
@@ -0,0 +1,327 @@
+<?php
+/* Template Name: Контакты */
+get_header();
+?>
+<div class="header-wrapper">
+
+    <header class="header">
+
+      <div class="container">
+
+        <div class="header__inner">
+
+          <div class="header__content">
+
+            <div class="header__logo">
+
+              <a class="header__logo-link" href="/">
+
+                FCQ
+
+              </a>
+
+            </div>
+
+            <div class="header__menu">
+
+              <div class="header__menu-item">
+
+                <a class="header__menu-link" href="/">Схема работы</a>
+
+              </div>
+
+              <div class="header__menu-item">
+
+                <a class="header__menu-link" href="/">Прайс</a>
+
+              </div>
+
+              <div class="header__menu-item">
+
+                <a class="header__menu-link" href="/">Доставка из китая</a>
+
+              </div>
+
+              <div class="header__menu-item">
+
+                <a class="header__menu-link" href="/">О нас</a>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div class="header__btns">
+
+            <div class="header__btn">
+
+              <a class="header__btn-link" href="">Калькулятор</a>
+
+            </div>
+
+            <div class="header__btn">
+
+              <a class="header__btn-link" href="#modal">Заказать звонок</a>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </header>
+
+    <section class="request">
+
+      <div class="container">
+
+        <h1 class="contacts__title">Контакты</h1>
+
+      </div>
+
+    </section>
+
+  </div>
+
+  <div class="wrapper">
+
+    <main class="main">
+
+      <section class="contacts">
+
+        <div class="container-fluid">
+
+          <div class="contacts__items">
+
+            <div class="contacts__item">
+
+              <h6 class="contacts__item-title">Служба клиентского сервиса</h6>
+
+              <p class="contacts__item-subtitle">Проконсультируем и ответим на любые вопросы по работе платформы</p>
+
+              <div class="contacts__item-info">
+
+                <a class="contacts__item-text" href="tel:79998888558">
+
+                  Позвонить
+
+                </a>
+
+                <p class="contacts__item-link">
+
+                  +7 (999) 888 8558
+
+                </p>
+
+              </div>
+
+              <div class="contacts__item-info">
+
+                <a class="contacts__item-text" href="mailto:fcq-fulfillment@yandex.ru">
+
+                  Написать
+
+                </a>
+
+                <div class="contacts__item-links">
+
+                  <p class="contacts__item-link" href="mailto:fcq-fulfillment@yandex.ru">
+
+                    Почта&nbsp; info@togotop.ru
+
+                  </p>
+
+                  <div class="contacts__item-social">
+
+                    <a class="contacts__item-link contacts__item-link--btn" href="#">
+
+                      <i>
+
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                          <g clip-path="url(#clip0_11_24)">
+
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+
+                              d="M11.9965 0C5.38113 0 0 5.38264 0 11.9999C0 14.6242 0.846321 17.058 2.285 19.0335L0.789795 23.4919L5.40222 22.0178C7.2993 23.2735 9.56321 24 12.0035 24C18.6189 24 24 18.6172 24 12.0001C24 5.38285 18.6189 0.000198364 12.0035 0.000198364L11.9965 0ZM8.64642 6.09541C8.41374 5.53814 8.23739 5.51704 7.88489 5.5027C7.76486 5.49573 7.63111 5.48877 7.48282 5.48877C7.02422 5.48877 6.54473 5.62276 6.25552 5.91902C5.90302 6.27879 5.02841 7.11819 5.02841 8.8396C5.02841 10.561 6.28379 12.2259 6.45297 12.4588C6.62932 12.6914 8.90039 16.2752 12.4266 17.7357C15.1842 18.8786 16.0025 18.7727 16.63 18.6387C17.5468 18.4412 18.6965 17.7636 18.9857 16.9455C19.2749 16.127 19.2749 15.4286 19.1901 15.2805C19.1055 15.1323 18.8726 15.0479 18.5201 14.8713C18.1676 14.6949 16.4537 13.8483 16.1292 13.7354C15.8118 13.6156 15.5086 13.658 15.269 13.9967C14.9304 14.4693 14.599 14.9492 14.3309 15.2383C14.1193 15.464 13.7736 15.4923 13.4846 15.3722C13.0966 15.2102 12.0107 14.8289 10.6705 13.6367C9.63369 12.7127 8.92848 11.5629 8.72406 11.2172C8.51944 10.8646 8.70294 10.6598 8.86496 10.4694C9.04131 10.2506 9.21052 10.0955 9.38687 9.89084C9.56322 9.68636 9.66195 9.58044 9.77481 9.34053C9.89483 9.10778 9.81003 8.86787 9.72543 8.69147C9.64084 8.51507 8.93563 6.79365 8.64642 6.09541Z"
+
+                              fill="#67C15E" />
+
+                          </g>
+
+                          <defs>
+
+                            <clipPath id="clip0_11_24">
+
+                              <rect width="24" height="24" fill="white" />
+
+                            </clipPath>
+
+                          </defs>
+
+                        </svg>
+
+
+
+                      </i>
+
+                      <u>WhatsApp</u>
+
+                    </a>
+
+                    <a class="contacts__item-link contacts__item-link--btn" href="#">
+
+                      <i>
+
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                          <g clip-path="url(#clip0_12_29)">
+
+                            <path
+
+                              d="M12 0C5.37253 0 0 5.37253 0 12C0 18.6275 5.37253 24 12 24C18.6275 24 24 18.6275 24 12C24 5.37253 18.6275 0 12 0Z"
+
+                              fill="#40B3E0" />
+
+                            <path
+
+                              d="M17.8388 6.90272L15.6954 17.709C15.6954 17.709 15.3957 18.4584 14.5714 18.0987L9.62532 14.3067L7.82682 13.4375L4.79926 12.4182C4.79926 12.4182 4.33463 12.2534 4.28963 11.8937C4.24473 11.534 4.81426 11.3392 4.81426 11.3392L16.8495 6.61791C16.8495 6.61791 17.8388 6.18328 17.8388 6.90272Z"
+
+                              fill="white" />
+
+                            <path
+
+                              d="M9.24549 17.5877C9.24549 17.5877 9.10112 17.5742 8.92121 17.0046C8.7414 16.4351 7.82715 13.4375 7.82715 13.4375L15.0962 8.82126C15.0962 8.82126 15.516 8.56645 15.501 8.82126C15.501 8.82126 15.5759 8.86616 15.3511 9.07598C15.1262 9.28588 9.64065 14.2169 9.64065 14.2169"
+
+                              fill="#D2E5F1" />
+
+                            <path
+
+                              d="M11.5222 15.7606L9.56586 17.5443C9.56586 17.5443 9.41295 17.6604 9.24561 17.5876L9.62023 14.2744"
+
+                              fill="#B5CFE4" />
+
+                          </g>
+
+                          <defs>
+
+                            <clipPath id="clip0_12_29">
+
+                              <rect width="24" height="24" fill="white" />
+
+                            </clipPath>
+
+                          </defs>
+
+                        </svg>
+
+
+
+                      </i>
+
+                      <u>Telegram</u>
+
+                    </a>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="contacts__item">
+
+              <h6 class="contacts__item-title">Техническая поддержка</h6>
+
+              <p class="contacts__item-subtitle">Поможем решить возникшие трудности в работе</p>
+
+              <a class="contacts__item-link contacts__item-link--btn" href="#">
+
+                <u>Создать обращение</u>
+
+              </a>
+
+            </div>
+
+            <div class="contacts__item">
+
+              <h6 class="contacts__item-title">Документация по платформе</h6>
+
+              <p class="contacts__item-subtitle contacts__item-subtitle--yt">Поможем решить возникшие трудности в работе
+
+              </p>
+
+              <p class="contacts__item-text">Ссылка на видео - </p>
+
+              <a class="contacts__item-link contacts__item-link--btn contacts__item-link--yt"
+
+                href="https://youtu.be/N1P3KNHidec">
+
+                <p>Ссылка на видео</p>
+
+                <u>https://youtu.be/N1P3KNHidec</u>
+
+              </a>
+
+            </div>
+
+            <div class="contacts__item">
+
+              <div class="contacts__item-box">
+
+                <div class="contacts__box-info">
+
+                  <h6 class="contacts__item-title">Партнёрство и сотрудничество</h6>
+
+                  <p class="contacts__item-subtitle">По вопросам взаимодействия, партнёрских программ
+
+                    и развития совместных проектов</p>
+
+                </div>
+
+                <div class="contacts__item-links">
+
+                  <a class="contacts__item-link" href="tel:+79295174678">+7 (939) 111-05-34</a>
+
+                  <a class="contacts__item-link" href="mailto:fcq-fulfillment@yandex.ru">Почта &nbsp; <u>partners@togotop.ru</u></a>
+
+                </div>
+
+              </div>
+
+              <div class="contacts__item-box">
+
+                <div class="contacts__box-info">
+
+                  <h6 class="contacts__item-title">Услуги под ключ</h6>
+
+                  <p class="contacts__item-subtitle">Поможем организовать процесс под ваш бизнес 
+и продвижение на Wildberries</p>
+
+                </div>
+
+                <div class="contacts__item-links">
+
+                  <a class="contacts__item-link" href="tel:+79295174678">+7 (939) 111-05-34</a>
+
+                  <a class="contacts__item-link" href="mailto:fcq-fulfillment@yandex.ru">Почта &nbsp; <u>info@togotop.ru</u></a>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+    </main>
+<?php get_footer(); ?>
